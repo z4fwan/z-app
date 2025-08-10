e95375a76344c049a7fdec73b86bdab866f4ef28
z4fwan <zn4.studio@gmail.com>
2025-08-10 12:45:34 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,8 +8,9 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
     httpOnly: true, // prevent XSS attacks
-    sameSite: "none", // allow cross-site cookies (needed for frontend-backend on different domains)
-    secure: process.env.NODE_ENV !== "development", // HTTPS only in production
+    sameSite: "none", // allow cross-site cookies
+    secure: process.env.NODE_ENV === "production", // true in production (and HTTPS)
+    path: "/", // match clearing path
   });
 
   return token;
